e4d68797ad0e57e2be2f3693a3df19cc120a4620
Tami Sweitzer <tamara0715@gmail.com>
2020-03-27 01:09:54 -0500

    Corrected logic error with the token in the authentification function. Tested adding a new user from Postman. Everything works

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -80,6 +80,7 @@ app.post("/signup", (req, res) => {
   };
 
   // Validate data
+  let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
     .then(doc => {
@@ -92,9 +93,21 @@ app.post("/signup", (req, res) => {
       }
     })
     .then(data => {
+      userId = data.user.uid;
       return data.user.getIdToken();
     })
-    .then(token => {
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId
+      };
+      // Persist into the collection
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
       return res.status(201).json({ token });
     })
     .catch(err => {
