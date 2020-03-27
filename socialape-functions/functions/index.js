a26b74b85b1af6ba2a0576df6d3f7429abde6125
Tami Sweitzer <tamara0715@gmail.com>
2020-03-26 23:39:51 -0500

    Added the signup route and create user function

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -72,5 +72,31 @@ app.post("/scream", (req, res) => {
     });
 });
 
+// Signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  // TODO validate data
+
+  // create the user
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res
+        .status(201)
+        .json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+});
+
 // ~/api/
 exports.api = functions.https.onRequest(app);
